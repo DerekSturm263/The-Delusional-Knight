8dd68c0b3f9db090e035a2bc3ce069455beb5560
Derek <dereksturm263@gmail.com>
2020-09-17 00:15:13 -0700

    Almost finished dialogue
    Oh yeah I added URP too.

---
+++ Assets/Derek's Stuff/AllDialogue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AllDialogue
+{
+    public static SpeechBubble firstConversation = new SpeechBubble(Characters.witch, "I see you’ve fallen right into my trap!");
+    public static SpeechBubble secondConversation = new SpeechBubble(Characters.witch, "You are going to die now!");
+    public static SpeechBubble thirdConversation = new SpeechBubble(Characters.player, "Not so fast!");
+
+    public static void Initialize()
+    {
+        firstConversation.SetLines(new List<SpeechBubble> { secondConversation });
+        secondConversation.SetLines(new List<SpeechBubble> { thirdConversation });
+        thirdConversation.SetLines();
+    }
+}
---
+++ Assets/Derek's Stuff/SpeechBubble.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubble
+{
+	public Character Speaker;
+	public string Dialogue;
+	public List<SpeechBubble> NextLines = new List<SpeechBubble>();
+
+	public SpeechBubble(Character speaker, string dialogue)
+	{
+		Speaker = speaker;
+		Dialogue = dialogue;
+	}
+
+	public void SetLines(List<SpeechBubble> nextLines = null)
+    {
+		NextLines = nextLines;
+    }
+}
