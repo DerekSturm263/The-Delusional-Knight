42ed568498df56e7806b0f1b486dfc6f99792f1d
Cody Nielsen <dragonsteve222@gmail.com>
2020-09-21 10:36:07 -0800

    10
    10

---
+++ Assets/Ineventory/Spawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject item;
+    private Transform player;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    public void SpawnDroppedItem()
+    {
+        Vector2 playerPos = new Vector2(player.position.x, player.position.y + 2);
+        Instantiate(item, playerPos, Quaternion.identity);
+    }
+}
