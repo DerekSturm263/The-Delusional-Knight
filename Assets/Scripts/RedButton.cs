cb817db3ca70625b7f5a4c2b72894d81369b02fe
Kendrick <momokoi321@gmail.com>
2020-09-20 20:36:08 -0700

    Created Interactable script, player direction
    e

---
+++ Assets/Scripts/RedButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedButton : Interactable
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    protected override void Interact()
+    {
+        base.Interact();
+        Debug.Log("redButtonPressed");
+    }
+
+}
