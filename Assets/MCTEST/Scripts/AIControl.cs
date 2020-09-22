05e59ac04fc7b8553caeda0132cd155ea649a231
MCGM12 <matocraft506@gmail.com>
2020-09-22 13:39:50 -0700

    Patrol Stuffs

---
+++ Assets/MCTEST/Scripts/AIControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class AIControl : MonoBehaviour
+{
+    
+    Patrol patrol;
+    AIPath aiPath;
+    EnemyAI enemyAI;
+
+    GameObject enemy, player;
+
+
+    void Start()
+    {
+        patrol = GetComponent<Patrol>();
+        aiPath = GetComponent<AIPath>();
+        enemyAI = GetComponent<EnemyAI>();
+
+        enemy = gameObject;
+        player = GameObject.FindGameObjectWithTag("Player");
+
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
