7137f46550cbc44dd2d093f88e104802dbcdfd48
Kendrick <momokoi321@gmail.com>
2020-09-22 21:03:39 -0700

    added Npc script

---
+++ Assets/Scripts/Npc.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Npc : Interactable
+{
+    private DialogueManager dm;
+    void Start()
+    {
+        dm = GameObject.FindObjectOfType<DialogueManager>();
+    }
+    protected override void Interact()
+    {
+        base.Interact();
+        dm.WriteDialogue(AllDialogue.firstConversation, Characters.player, Characters.witch);
+    }
+}
