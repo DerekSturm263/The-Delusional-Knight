13550e1d02bdb0c4d9e9d03ebf15e8a02b0069e3
Derek Sturm <dereksturm263@gmail.com>
2020-09-24 22:19:20 -0700

    Tilemap
    F

--- Assets/Scripts/Disappear.cs
+++ Assets/Scripts/Disappear.cs
@@ -4,30 +4,58 @@ using UnityEngine;
 
 public class Disappear : MonoBehaviour
 {
-    private GameObject player;
+    public enum Room
+    {
+        Outside, Outer_Ring, Guards_Room, Kitchen, Throne_Room, Storage_Room, Kings_Room, Princess_Room, Outer_Room_2
+    }
+    private static Room currentRoom;
+    private static Room oldRoom;
+
+    public static Dictionary<Room, Disappear> rooms = new Dictionary<Room, Disappear>();
+
     private Animator anim;
+    public Room thisRoom;
+    public bool startsVisible;
 
     private void Awake()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
         anim = GetComponent<Animator>();
-    }
+        rooms.Add(thisRoom, this);
 
-    private void Update()
-    {
-        if (player.transform.position.y > transform.position.y)
+        if (!startsVisible)
             FadeOut();
-        else
-            FadeIn();
     }
 
-    private void FadeOut()
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Player entered " + gameObject.name);
+
+        if (col.CompareTag("Player"))
+            ChangeRoom();
+    }
+
+    private void ChangeRoom()
+    {
+        oldRoom = currentRoom;
+        currentRoom = thisRoom;
+
+        if (oldRoom == currentRoom)
+            return;
+
+        Debug.Log(oldRoom);
+        Debug.Log(currentRoom);
+
+        rooms[currentRoom].FadeIn();
+        rooms[oldRoom].FadeOut();
+    }
+
+    public void FadeOut()
     {
         anim.SetBool("Exit", true);
         anim.SetBool("Enter", false);
     }
 
-    private void FadeIn()
+    public void FadeIn()
     {
         anim.SetBool("Enter", true);
         anim.SetBool("Exit", false);
